736bca54c4d61567bc80aae4132bd2b9a67f43e1
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-10 11:07:31 +0900

    [level 2] Title: 오픈채팅방, Time: 77.42 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] record) {
+        int count = 0;
+        HashMap<String, String> map = new HashMap<>();
+        for(int i = 0; i < record.length; i++){
+            String[] temp = record[i].split(" ");
+            
+            if(temp[0].equals("Leave"))
+                continue;
+            else if(temp[0].equals("Enter"))
+                map.put(temp[1], temp[2]);
+            else{
+                map.put(temp[1], temp[2]);
+                count++;
+            }
+        }
+        String[] answer = new String[record.length - count];
+        int index = 0;
+        
+        for(int i = 0; i < record.length; i++){
+            String[] temp = record[i].split(" ");
+            String name = map.get(temp[1]);
+            
+            if(temp[0].equals("Enter"))                     
+                answer[index++] = name + "님이 들어왔습니다.";
+            else if(temp[0].equals("Leave"))
+                answer[index++] = name + "님이 나갔습니다.";
+        }
+        return answer;
+    }
+}
