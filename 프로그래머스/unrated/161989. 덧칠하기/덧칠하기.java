fc0abeb797f02151960d02b1212f71acdf74294d
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-15 11:06:13 +0900

    [unrated] Title: 덧칠하기, Time: 0.83 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,13 +2,10 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         int paintIdx = 0;
-        
-        for (int i = 0; i < section.length; i++) {
-            if (section[i] < paintIdx) {
+        for(int i = 0; i < section.length; i++){
+            if(section[i] < paintIdx)
                 continue;
-            }
-
-            answer += 1;
+            answer++;
             paintIdx = section[i] + m;
         }
         return answer;
