778ca6318d40390abfe5aae3be53d26c7eb3525b
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-20 11:21:52 +0900

    [level 2] Title: 튜플, Time: 0.47 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        ArrayList<Integer> result = new ArrayList<>();
+        s = s.substring(2,s.length());
+        s = s.substring(0,s.length()-2).replace("},{","#");
+        String str[] = s.split("#");
+        Arrays.sort(str, (String s1, String s2) -> s1.length() - s2.length());
+        for(int i = 0; i < str.length; i++){
+            String[] x = str[i].split(",");
+            for(int j = 0; j < x.length; j++){
+                int number = Integer.parseInt(x[j]);
+                if(!result.contains(number))
+                    result.add(number);
+            }
+        }
+        //Integer[] answer = result.toArray(new int[0]);
+        //int[] answer = result.stream().toArray(int[]::new);
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+}
