9addaff1cb0e4f796a54d8a935178224ec1f5b68
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-20 11:38:03 +0900

    [level 2] Title: 튜플, Time: 0.70 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -7,9 +7,9 @@ class Solution {
         String str[] = s.split("#");
         Arrays.sort(str, (String s1, String s2) -> s1.length() - s2.length());
         for(int i = 0; i < str.length; i++){
-            String[] x = str[i].split(",");
-            for(int j = 0; j < x.length; j++){
-                int number = Integer.parseInt(x[j]);
+            String[] temp = str[i].split(",");
+            for(int j = 0; j < temp.length; j++){
+                int number = Integer.parseInt(temp[j]);
                 if(!result.contains(number))
                     result.add(number);
             }
