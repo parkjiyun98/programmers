8ccdb4ba47634c025c96244fe0e9a8a72d44a212
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-22 10:55:12 +0900

    [level 3] Title: 야근 지수, Time: 32.46 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        int sum = 0;
+        
+		for(int i=0; i<works.length; i++)
+			sum += works[i];
+
+		if(n >= sum) 
+			return 0;
+
+		while(n != 0) {
+			int max = 0;
+
+			for(int i=0; i<works.length; i++) 
+				if(max < works[i])	max = works[i];
+
+            for(int i=0; i<works.length; i++) {
+                if(works[i] == max) {
+                    works[i]--;
+                    n--;
+                    if(n == 0) break;
+                }
+            }
+		}
+
+		for(int i=0; i<works.length; i++)
+			answer += works[i] * works[i];
+
+		return answer;
+    }
+}
