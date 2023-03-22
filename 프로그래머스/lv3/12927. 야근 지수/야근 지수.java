8a34ca294b5c997786cc39b5e285f68836a6dd24
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-22 11:23:07 +0900

    [level 3] Title: 야근 지수, Time: 35.00 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -4,30 +4,32 @@ class Solution {
         long answer = 0;
         int sum = 0;
         
-		for(int i=0; i<works.length; i++)
-			sum += works[i];
-
-		if(n >= sum) 
-			return 0;
-
-		while(n != 0) {
-			int max = 0;
-
-			for(int i=0; i<works.length; i++) 
-				if(max < works[i])	max = works[i];
-
-            for(int i=0; i<works.length; i++) {
-                if(works[i] == max) {
+        for(int i = 0; i < works.length; i++)
+            sum += works[i];
+        if(n >= sum)
+            return 0;
+        
+        while(n != 0){
+            int max = 0;
+            
+            for(int i = 0; i < works.length; i++){
+                if(max < works[i])
+                    max = works[i];
+            }
+            
+            for(int i = 0; i < works.length; i++){
+                if(works[i] == max){
                     works[i]--;
                     n--;
                     if(n == 0) break;
                 }
             }
-		}
-
-		for(int i=0; i<works.length; i++)
-			answer += works[i] * works[i];
-
-		return answer;
+        }
+        
+        for(int i = 0; i < works.length; i++){
+            answer += works[i] * works[i];
+        }
+        
+        return answer;
     }
 }
