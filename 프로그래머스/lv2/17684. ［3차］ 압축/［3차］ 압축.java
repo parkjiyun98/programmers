0167e4b228eb22f1f5995dff9efa18495a7b4cd7
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-23 11:04:21 +0900

    [level 2] Title: [3차] 압축, Time: 14.81 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,18 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(String msg) {
-        ArrayList<Integer> array = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
         
-        String al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for(int i = 0; i < 26; i++){
-            map.put(al.charAt(i) + "", i+1);
+            map.put(s.charAt(i) + "", i+1);
         }
+        
         int start = 27;
         boolean check = false;
+        
         for(int i = 0; i < msg.length(); i++){
             String temp = msg.charAt(i) + "";
-
+            
             while(map.containsKey(temp)){
                 i++;
                 if(i == msg.length()){
@@ -22,17 +24,20 @@ class Solution {
                 temp += msg.charAt(i);
             }
             if(check){
-                array.add(map.get(temp));
+                list.add(map.get(temp));
                 break;
             }
+            
             map.put(temp, start++);
-            array.add(map.get(temp.substring(0, temp.length()-1)));
+            list.add(map.get(temp.substring(0, temp.length() - 1)));
             i--;
         }
-        int[] answer = new int[array.size()];
-        for(int i = 0; i < array.size(); i++){
-            answer[i] = array.get(i);
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
