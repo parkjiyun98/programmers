b33c803eb96fc34db0c14ecc857776b7cc752cac
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-23 10:05:38 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+		
+		return dfs(k, dungeons, visited, 0);
+    }
+    public int dfs(int k, int[][] dungeons, boolean[] visited, int count){
+        for(int i = 0; i < dungeons.length; i++){
+            if(k >= dungeons[i][0] && !visited[i]){
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, visited, count + 1);
+                visited[i] = false;
+            }  
+        }
+        answer = Math.max(answer, count);
+        return answer;
+    }
+}
