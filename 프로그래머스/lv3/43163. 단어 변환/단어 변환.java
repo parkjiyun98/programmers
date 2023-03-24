7250dbe3855d348f3012fefa4a2935b8a7851726
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-24 10:19:33 +0900

    [level 3] Title: 단어 변환, Time: 0.07 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.*;
 class Solution {
-    int ans = 0;
+    int answer = 0;
     public int solution(String begin, String target, String[] words) {
         List<String> list = new ArrayList<>(Arrays.asList(words));
         if(!list.contains(target)) return 0;
         boolean[] visited = new boolean[words.length];
-        dfs(begin,target,words,0,visited);
-        return ans;
-
+        dfs(begin, target, words, visited, 0);
+        
+        return answer;
     }
-    public void dfs(String begin, String target,String[] words, int depth,boolean[] visited){
-        if(begin.equals(target)) {
-            ans = depth;
+    
+    public void dfs(String begin, String target, String[] words, boolean[] visited, int depth){
+        if(begin.equals(target)){
+            answer = depth;
             return;
         }
-            for(int i = 0; i < words.length; i++){
-                if(visited[i]) continue;
-                if(check(begin,words[i])){
-                    visited[i] = true;
-                    dfs(words[i],target,words,depth +1,visited);
-                    visited[i] = false;
-                }
+        
+        for(int i = 0; i < words.length; i++){
+            if(visited[i]) continue;
+            if(check(begin, words[i])){
+                visited[i] = true;
+                dfs(words[i], target, words, visited, depth + 1);
+                visited[i] = false;
             }
+        }
     }
-    public static boolean check(String s1, String s2){
+    
+    public boolean check(String s1, String s2){
         int count = 0;
-
-        for(int i = 0; i < s1.length() ; i++){
+        
+        for(int i = 0; i < s1.length(); i++){
             if(s1.charAt(i) != s2.charAt(i))
                 count++;
         }
+        
         return count == 1;
-
     }
 }
