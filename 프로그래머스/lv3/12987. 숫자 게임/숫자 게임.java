e67e8405cf532b483b41b31d1ad4b869173d7f0f
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-28 10:08:30 +0900

    [level 3] Title: 숫자 게임, Time: 60.34 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int a = 0;
+        int b = 0;
+        while (a < A.length && b < B.length) {
+            if (A[a] < B[b]) {
+                answer++;
+                a++;
+                b++;
+            }
+            else b++;
+        }
+        return answer;
+    }
+}
