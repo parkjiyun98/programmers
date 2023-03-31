35aebaa288d036ecf82ff7711610d570d3ee8808
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-03-31 09:40:52 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.32 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,24 +1,21 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        answer = new_id.toLowerCase().replaceAll("[^\\w\\-_.]", "");
-        answer = answer.replaceAll("\\.{2,}", ".").replaceAll("^[.]|[.]$","");
-        if(answer.length()>15){
-            answer = answer.substring(0,15);
-        }
+        String answer = new_id.toLowerCase(); // 소문자로 
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); //-_. 영문자 숫자만 남김 
+        answer = answer.replaceAll("[.]{2,}", "."); // .2개 이상 .으로 
+        answer = answer.replaceAll("^[.]|[.]$", ""); // 처음과 끝 . 제거 
 
-        answer = answer.replaceAll("[.]$", "");
-
-        if(answer.equals("")){
-            answer = "aaa";
-            return answer;
+        if(answer.equals("")) // 빈 문자열이라면 a 추가 
+            answer += "a";
+            
+        if(answer.length() >= 16){ // 16자 이상이면 15자로 
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("^[.]|[.]$", ""); // 끝 . 제거 
         }
+        if(answer.length() <= 2) // 2자 이하라면 3자까지 마지막 문자추가 
+            while(answer.length() < 3)
+                answer += answer.charAt(answer.length() - 1);
 
-        if(answer.length()<=2){
-            while (answer.length()<3){
-                answer += answer.charAt(answer.length()-1);
-            }
-        }
         return answer;
     }
 }
