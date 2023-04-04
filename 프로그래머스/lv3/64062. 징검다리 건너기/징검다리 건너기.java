3c27bfcefd1bdc6408609da5ced0658317531e5d
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-04 11:45:21 +0900

    [level 3] Title: 징검다리 건너기, Time: 16.32 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,21 +1,28 @@
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
+        //징검다리 건널 친구 최소
         int min = 0;
+        //징검다리 건널 친구 최대
         int max = Integer.MAX_VALUE;
+        
         while(min <= max){
+            //징검다리 건널 수 있는 친구
             int mid = (min + max) / 2;
             if(check(stones, k, mid)){
+                //다리를 건널 수 있으면 친구 수 늘림
                 min = mid + 1;
                 answer = mid;
             }
             else
+                //다리 건널 수 없으면 친구 수 줄임
                 max = mid - 1;
                 
         }
         return answer;
     }
     public boolean check(int[] stones, int k, int mid){
+        //못 건너는 징검다리 개수
         int count = 0;
         for(int i = 0; i < stones.length; i++){
             if(stones[i] < mid){
