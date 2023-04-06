16ea8fb5cfa027a077f238ff7e0f51b23143d381
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-06 09:17:56 +0900

    [level 3] Title: 섬 연결하기, Time: 0.67 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        int[] set = new int[n];
+        for (int i = 0; i < n; i++) {
+            set[i] = i;
+        }
+        Arrays.sort(costs, (a, b) -> a[2]-b[2]); 
+        for (int i = 0; i < costs.length; i++) {
+        if (find(set, costs[i][0]) != find(set, costs[i][1])) { 
+                set[find(set, costs[i][1])] = find(set, costs[i][0]) ; 
+                answer = answer + costs[i][2]; 
+            }
+        }
+        return answer;
+    }
+    
+    public int find(int[] set, int node) {
+        if (set[node] == node) {
+            return node;
+        }
+        return find(set, set[node]);
+    }
+}
