5613d3039cd66b13e480614f28cff9320ba8559f
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-12 09:52:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.35 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int n) {
+        /*
         int answer = 1;
         int i = 1, start = 1, sum = 0;
         while(start <= n/2){
@@ -16,6 +17,25 @@ class Solution {
                 start++;
                 i = start;
                 sum = 0;
+            }
+        }
+        return answer;
+        */
+        if(n == 2 || n == 1)
+            return 1;
+        int answer = 1, sum = 0, lt = 0;
+        int m = n/2 + 1;
+        int[] arr = new int[m];
+        for(int i = 0; i < m; i++){
+            arr[i] = i+1;
+        }
+        for(int rt = 0; rt < m; rt++){
+            sum += arr[rt];
+            if(sum == n)
+                answer++;
+            while(sum >= n){
+                sum -= arr[lt++];
+                if(sum == n) answer++;
             }
         }
         return answer;
