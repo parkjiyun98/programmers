bc51a3b68b73e073e626e13e65196db9e48df7e0
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-17 16:53:30 +0900

    [level 3] Title: 거스름돈, Time: 21.69 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n, int[] money) {
-        int[] answer = new int[n+1];
+        int[] answer = new int[100001];
         answer[0] = 1;
         for(int i : money) {
             for(int j = i; j <= n; j++) {
                 answer[j] += answer[j-i];
             }
         }
-        return answer[n];
+        return answer[n] % 1000000007;
     }
 }
