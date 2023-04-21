c2c760c589657428830c96b119f15da482bd5aec
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-21 11:27:57 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+        /*
         int answer = 0;
         int count = 0;
         HashMap<String, Integer> map = new HashMap<>();
@@ -25,6 +26,22 @@ class Solution {
                     answer++;
                 count++;
             }
+        }
+        return answer;
+        */
+        int answer = 0;
+        for(int i=0;i<discount.length-9;i++) {
+            int j;
+            for(j=0;j<want.length;j++) {
+                int check=0;
+                for(int k=i;k<i+10;k++) {
+                    if(discount[k].equals(want[j])) check++;
+                }        
+                if(check<number[j]) {
+                     break;
+                }
+            }
+           if(j==want.length) answer ++;
         }
         return answer;
     }
