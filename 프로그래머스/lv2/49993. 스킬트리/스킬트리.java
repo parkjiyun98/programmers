ca31caab2cdfd6f8b1728d32adda9517c3f7fd25
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-04-24 11:28:39 +0900

    [level 2] Title: 스킬트리, Time: 0.19 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(String skillTree : skill_trees){
+            String temp = skillTree;
+            
+            for(int i = 0; i < skillTree.length(); i++){
+                String s = skillTree.substring(i, i+1);
+                if(!skill.contains(s))
+                    temp = temp.replace(s, "");
+            }
+            
+            if(skill.indexOf(temp) == 0)
+                answer++;
+        }
+        return answer;
+    }
+}
