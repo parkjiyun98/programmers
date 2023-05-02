1ac0d0ccd0f290cda29dd9754f9f4010e91c6b2a
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-05-02 11:00:51 +0900

    [level 2] Title: 주식가격, Time: 27.20 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,7 +1,8 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
+        /*
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
                 answer[i]++;
@@ -9,7 +10,21 @@ class Solution {
                     break;
             }
         }
-        
+        */
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop(); // 답을 구했기 때문에 stack에서 제거
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) { // 값을 구하지 못한 index == 끝까지 가격이 떨어지지 않은 주식
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
         return answer;
     }
 }
