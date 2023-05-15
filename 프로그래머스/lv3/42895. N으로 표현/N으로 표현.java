4e639feebac55eaa6ad543826772fd4f65b58dae
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-05-15 10:42:53 +0900

    [level 3] Title: N으로 표현, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -1,40 +1,36 @@
 import java.util.*;
 class Solution {
-    public int solution(int N, int number) {
-        List<Set<Integer>> countList = new ArrayList<>();
-
-        for(int i=0; i<9; i++)
-            countList.add(new HashSet<>());
-
-        countList.get(1).add(N); // N을 1개 쓴 값은 N 혼자이다.
-
-        for(int i=2; i<9; i++){
-            Set<Integer> countSet = countList.get(i);
-
-            for(int j=1; j<=i; j++){
-                Set<Integer> preSet = countList.get(j);
-                Set<Integer> postSet = countList.get(i - j);
-
-                for(int preNum : preSet){
-                    for(int postNum : postSet){
-                        countSet.add(preNum + postNum);
-                        countSet.add(preNum - postNum);
-                        countSet.add(preNum * postNum);
-
-                        if(preNum != 0 && postNum != 0)
-                            countSet.add(preNum / postNum);
-                    }
-                }
+    //순서쌍 a에 b 합치기
+    public void unionSet(Set<Integer> union, Set<Integer> a, Set<Integer> b) {
+        for (int n1 : a) {
+            for (int n2 : b) {
+                union.add(n1 + n2);
+                union.add(n1 - n2);
+                union.add(n1 * n2);
+                if (n2 != 0)
+                    union.add(n1 / n2);
             }
-
-            countSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
         }
-
-        for(Set<Integer> sub : countList){
-            if(sub.contains(number))
-                return countList.indexOf(sub);
+    }
+    public int solution(int N, int number) {
+        List<Set<Integer>> setList = new ArrayList<>();
+        
+        for (int i = 0; i < 9; i++)
+            setList.add(new HashSet<>()); // 개수 별 해쉬셋
+        setList.get(1).add(N); //1개로 만들 수 있는 건 나 자신뿐
+        if (number == N)
+            return 1;
+        for (int i = 2; i < 9; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                unionSet(setList.get(i), setList.get(i - j), setList.get(j));
+                unionSet(setList.get(i), setList.get(j), setList.get(i - j));
+            }
+            String n = Integer.toString(N);
+            setList.get(i).add(Integer.parseInt(n.repeat(i))); //연속된 숫자 넣기
+            for (int num : setList.get(i))
+                if (num == number)
+                    return i;
         }
-
         return -1;
     }
 }
