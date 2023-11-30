14ea620a350677f4d4017c3784b5fc41cd22761e
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-11-30 21:46:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.35 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
+        /*
         int[] answer = new int[score.length];
         
         for(int i = 0; i < score.length; i++){
@@ -12,6 +13,24 @@ class Solution {
             else
                 answer[i] = integerTemp[k-1];
         }
+        return answer;
+        */
+        int[] answer = new int[score.length];
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+
+            answer[i] = priorityQueue.peek();
+        }
+        
         return answer;
     }
 }
